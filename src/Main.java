03593bc3ff422537873cd219711347d135c1e04b
Sivert <s315325@stud.hioa.no>
2017-10-24 12:50:23 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.util.Arrays;
+
+/**
+ * Created by Sivert on 24.10.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("- Main");
+
+        String[] s = {"Ole", "Per", "gustav", "hv"};
+        Liste<String> liste = new DobbeltLenketListe<>(s);
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+}
